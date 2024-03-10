72f0423458a603b1b2234afe4eb0f0a6b5605b4a
heathernfran <heathernfran@gmail.com>
2024-03-10 12:15:08 -0700

    Connect useCourses hook with Card component for selected course id state

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,20 +1,44 @@
+import type { Course } from "@/app/lib/definitions";
+import { formatTimeZone } from "@/app/lib/utilities";
 import Dates from "@/app/components/Card/components/Dates";
 import Instructor from "@/app/components/Card/components/Instructor";
 import Pricing from "@/app/components/Card/components/Pricing";
 import Times from "@/app/components/Card/components/Times";
-import type { Course } from "@/app/lib/definitions";
-import { formatTimeZone } from "@/app/lib/utilities";
+import { useCourses } from "@/app/context/CoursesContext";
 import "./Card.css";
 
 export default function Card({ course }: { course: Course }) {
+  const { state, dispatch } = useCourses();
+
   const { dates, id, location, instructors, pricing } = course;
 
+  function handleChange() {
+    // setIsChecked(!isChecked);
+    // onChange(id);
+    dispatch({ type: "UPDATE_SELECTED_COURSE_ID", id });
+  }
+
+  // console.log("id === state.selectedCourseId", id === state.selectedCourseId);
+
   return (
-    <div className="card">
+    <div
+      className={`card ${
+        id === state.selectedCourseId
+          ? "border-blue-50"
+          : "border-neutral-80 hover:border-primary-20"
+      }`}
+    >
       {/* Begin left column */}
       <div>
         <div className="mb-4 mt-6 ml-2">
-          <input type="radio" id={id.toString()} name="course" value={id} />
+          <input
+            type="radio"
+            id={id.toString()}
+            name="course"
+            value={id}
+            checked={id === state.selectedCourseId}
+            onChange={handleChange}
+          />
           <label
             htmlFor={id.toString()}
             className="text-primary-30 font-bold ml-2"
