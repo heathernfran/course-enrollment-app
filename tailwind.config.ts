dc31002abe39505e1b59eadbc41a2f722bac1fed
heathernfran <heathernfran@gmail.com>
2024-03-10 11:30:24 -0700

    Extend tailwind theme with custom colors and font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,53 @@
 import type { Config } from "tailwindcss";
 
+/**
+ * colors:
+ * neutral_0: #000000
+ * neutral_100: #FFFFFF
+ * neutral_80: #CCCCCC
+ * primary_20: #4D0001
+ * primary_30: #600C20
+ * primary_60: #ED0000
+ * primary_40: #A61518
+ * blue_50: #0073DD
+ *
+ * font-size/line-height:
+ * title-large: 34/38
+ * title-small: 30/38
+ * heading-medium: 24/32
+ * heading-small: 20/26
+ * body-medium: 18/24
+ */
+
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        neutral: {
+          80: "#CCCCCC",
+        },
+        blue: {
+          50: "#0073DD",
+        },
+        primary: {
+          20: "#4D0001",
+          30: "#600C20",
+          40: "#A61518",
+          60: "#ED0000",
+        },
+      },
+      fontSize: {
+        "title-large": ["34px", "38px"],
+        "title-small": ["30px", "38px"],
+        "heading-medium": ["24px", "32px"],
+        "heading-small": ["20px", "26px"],
+        "body-medium": ["18px", "24px"],
       },
     },
   },
