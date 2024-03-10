7ef2950c35fb35da04e436e91d27cd3104f042f3
heathernfran <heathernfran@gmail.com>
2024-03-10 11:33:41 -0700

    Apply styles and components to Card component

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,39 +1,41 @@
-import { Course } from "@/app/lib/definitions";
-import Image from "next/image";
+import Dates from "@/app/components/Card/components/Dates";
+import Instructor from "@/app/components/Card/components/Instructor";
+import Pricing from "@/app/components/Card/components/Pricing";
+import Times from "@/app/components/Card/components/Times";
+import type { Course } from "@/app/lib/definitions";
+import { formatTimeZone } from "@/app/lib/utilities";
+import "./Card.css";
 
 export default function Card({ course }: { course: Course }) {
   const { dates, id, location, instructors, pricing } = course;
 
   return (
-    <div>
+    <div className="card">
       {/* Begin left column */}
       <div>
-        <input type="radio" id={id.toString()} name="course" value={id} />
-        <label htmlFor={id.toString()}>Virtual Course</label>
-        <div>
-          {dates[0][0]} - {dates[0][1]}
+        <div className="mb-4 mt-6 ml-2">
+          <input type="radio" id={id.toString()} name="course" value={id} />
+          <label
+            htmlFor={id.toString()}
+            className="text-primary-30 font-bold ml-2"
+          >
+            Virtual Course
+          </label>
         </div>
-        <div>{location.timezone}</div>
-        <div>
-          {pricing.amount} {pricing.currency} Until {pricing.valid_until}
+        <div className="mx-8">
+          <Dates dates={dates} />
+          <Times dates={dates} location={location} />
+          <div className="my-2">{`${formatTimeZone(
+            location.timezone
+          )} Time`}</div>
+          <Pricing location={location} pricing={pricing} />
         </div>
       </div>
       {/* End left column */}
       {/* Begin right column */}
       <div>
         {instructors.map((instructor, index) => (
-          <div key={index}>
-            <Image
-              src={instructor.portrait_image}
-              alt={`Photo of ${instructor.first_name} ${instructor.last_name}`}
-              width={100}
-              height={100}
-            />
-            <div>
-              Instructor: <br />
-              {instructor.first_name}
-            </div>
-          </div>
+          <Instructor key={index} instructor={instructor} />
         ))}
       </div>
       {/* End right column */}
